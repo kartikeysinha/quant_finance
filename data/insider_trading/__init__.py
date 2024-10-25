b3453e1409f20325fa50cbbe8ff8223a5a34517d
kartikeysinha <7ktk10@gmail.com>
2024-10-24 21:41:36 -0400

    Updating documentation to reflect permission errors

--- data/insider_trading/__init__.py
+++ data/insider_trading/__init__.py
@@ -1,22 +1,10 @@
 """
 Author: @kartikeysinha
 
-Get the data directly from the SEC's website.
-https://www.sec.gov/data-research/sec-markets-data/insider-transactions-data-sets#:~:text=Section%2016%20of%20the%20Securities,transactions%20in%20the%20company's%20stock.
+Method 1: Scrape data from SEC's website 
+    This yields 403 error.
 
-Each quarter has a separate zip file. Each zip file contains multiple csvs:
-
-- DERIV_HOLDING.tsv
-- DERIV_TRANS.tsv
-- FOOTNOTES.tsv
-- FORM_345_metadata.json
-- FORM_345_readme.htm
-- NONDERIV_HOLDING.tsv
-- NONDERIV_TRANS.tsv
-- OWNER_SIGNATURE.tsv
-- REPORTINGOWNER.tsv
-- SUBMISSION.tsv
-
+https://web.archive.org/web/20240930103816/https://finviz.com/insidertrading.ashx
 """
 
 
