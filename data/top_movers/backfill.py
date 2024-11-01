c0c0cd0c5d7160d459caa1d10e6a73aac1ba6603
kartikeysinha <7ktk10@gmail.com>
2024-10-31 21:52:33 -0400

    First pass soln to getting past data regarding top movers

--- data/top_movers/backfill.py
+++ data/top_movers/backfill.py
@@ -6,19 +6,49 @@ This is imperfect as wayback doesn't always have
 """
 
 import datetime as dt
+import pandas as pd
 from waybackpy import WaybackMachineCDXServerAPI
 
 import quant_finance.data.top_movers.scrape as sc
 
 
-def backfill_top_movers(start_date, end_date):
+def backfill_top_movers(start_date, end_date, base_url : str = "https://thestockmarketwatch.com/markets/pre-market/today.aspx"):
+    """
+    
+    """
 
-    # generate weekdays form start to end date
+    # initializations
+    wbm = WaybackMachineCDXServerAPI(base_url, user_agent="Mozilla/5.0 (Windows NT 5.1; rv:40.0) Gecko/20100101 Firefox/40.0")
+    cur_date = start_date
+    frames = []
 
-    # for each weekdate
-        # get the 'nearest' url
-        # check if the 'nearest' url is of that day itself. If not, go to the next date, i.e. continue
-        # using sc.scrape_data with the url and date to get that day's data.
+    while cur_date <= end_date:
+        # Skip to next iteration if cur_date is a weekend
+        if cur_date.weekday() >= 5:
+            cur_date = cur_date + dt.timedelta( days=1 )        # todo: If Saturday, you can just add 2. Edge case is when we start with Sunday.
+            continue
+
+        # get the nearest url
+        nearest_url = wbm.near(year=cur_date.year, month=cur_date.month, day=cur_date.day)
+
+        # check if the url is of that day itself
+        if nearest_url.datetime_timestamp.date() != cur_date.date():
+            print(nearest_url.datetime_timestamp.date(), cur_date)
+            cur_date = cur_date + dt.timedelta( days=1 )
+            continue
+
+        # get data
+        cur_date_df = sc.scrape_data(url=nearest_url.archive_url, run_date=cur_date)
+
+        print(cur_date_df.head(2))
+
         # Store data so it can be combined later.
+        frames.append(cur_date_df)
+
+    if len(frames) == 0:
+        print("No relevant data found.")
+        return pd.DataFrame()
+
+    return pd.concat(frames, axis=0)        
 
 
