1185d50835d2b3d4585de77110a70760efd49bf9
kartikeysinha <7ktk10@gmail.com>
2024-10-31 22:04:10 -0400

    Minor fixes - requests timeout rn - need to fix

--- data/top_movers/backfill.py
+++ data/top_movers/backfill.py
@@ -23,27 +23,35 @@ def backfill_top_movers(start_date, end_date, base_url : str = "https://thestock
     frames = []
 
     while cur_date <= end_date:
+        print(cur_date)
         # Skip to next iteration if cur_date is a weekend
         if cur_date.weekday() >= 5:
             cur_date = cur_date + dt.timedelta( days=1 )        # todo: If Saturday, you can just add 2. Edge case is when we start with Sunday.
             continue
 
         # get the nearest url
-        nearest_url = wbm.near(year=cur_date.year, month=cur_date.month, day=cur_date.day)
+        try:
+            nearest_url = wbm.near(year=cur_date.year, month=cur_date.month, day=cur_date.day)
+        except:
+            cur_date = cur_date + dt.timedelta( days=1 )
+            continue
 
         # check if the url is of that day itself
         if nearest_url.datetime_timestamp.date() != cur_date.date():
-            print(nearest_url.datetime_timestamp.date(), cur_date)
             cur_date = cur_date + dt.timedelta( days=1 )
             continue
 
         # get data
-        cur_date_df = sc.scrape_data(url=nearest_url.archive_url, run_date=cur_date)
-
-        print(cur_date_df.head(2))
+        try:
+            cur_date_df = sc.scrape_data(url=nearest_url.archive_url, run_date=cur_date)
+        except:
+            cur_date = cur_date + dt.timedelta( days=1 )
+            continue
 
         # Store data so it can be combined later.
         frames.append(cur_date_df)
+
+        cur_date = cur_date + dt.timedelta( days=1 )
 
     if len(frames) == 0:
         print("No relevant data found.")
