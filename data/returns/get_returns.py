a94c13e82bebebed1f34abf6ed13c2f000c09287
kartikeysinha <7ktk10@gmail.com>
2024-11-08 19:01:02 -0500

    Adding more logic to get cnadles and returns from yfinance

--- data/returns/get_returns.py
+++ data/returns/get_returns.py
@@ -17,8 +17,13 @@ Right now, we only focus on daily data.
 
 import pandas as pd
 import yfinance as yf
+import datetime as dt
 
-def get_returns_from_yf(tickers, store_data=False):
+from quant_finance import *
+import quant_finance.data.utilities as data_utils
+
+
+def get_cnadles_from_yf(tickers, store_data=False):
     """
     ...
     """
@@ -33,12 +38,30 @@ def get_returns_from_yf(tickers, store_data=False):
     # Format the dataframe as desired
     df.index = df.index.swaplevel()     # ensure the ticker is the first index
     df.sort_index(inplace=True)
+    df.columns.name = None
 
     if store_data:
-        pass
+        data_utils.update_stored_data(
+            new_data=df, file_path=DATA_DIR+'returns/archive/', file_name='candles', cols_to_compare=['Ticker', 'Date', 'Adj. Close']
+        )
     
     return df
 
 
+def get_returns(tickers : str | list[str], start_date : str | dt.datetime, end_date : str | dt.datetime):
+    """
+    Calculated based on the Adj. Close prices for the ticker.
+    """
 
+    # read returns
+    ...
+
+    # check if all tickers exist between start_date and end_date in dataframe
+    ...
+
+    # If any don't exist, try installing them from yahoo finances
+    ...
+
+    # return the tickers within the start and end date
+    ...
 
